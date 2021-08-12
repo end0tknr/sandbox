4aed7d77191fcbf2543637c302bc316fd56fc52e
end0tknr <end0tknr@gmail.com>
2021-08-12 15:42:27 +0900

    Add files via upload

--- threejs_cannonjs_3d_maze/cannonjs_3d_maze.js
+++ threejs_cannonjs_3d_maze/cannonjs_3d_maze.js
@@ -109,7 +109,7 @@ var ground_texture = THREE.ImageUtils.loadTexture( 'img/ground.jpg' );
 ground_texture.wrapS = ground_texture.wrapT = THREE.RepeatWrapping;
 ground_texture.repeat.set( 64, 64 );
 
-var wall_texture = THREE.ImageUtils.loadTexture( 'img/images/wall.jpg' );
+var wall_texture = THREE.ImageUtils.loadTexture( 'img/wall.jpg' );
 
 var world = setPhysics(); //物理情報設定
 
