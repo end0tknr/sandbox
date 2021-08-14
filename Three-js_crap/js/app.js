df2e9960765c8f51e2423c610c10f2657a53092a
end0tknr <end0tknr@gmail.com>
2021-08-14 11:56:09 +0900

    Add files via upload

--- Three-js_crap/js/app.js
+++ Three-js_crap/js/app.js
@@ -50,96 +50,101 @@ document.getElementById("stage").appendChild(renderer.domElement);
 //==========
 // Web Audio
 navigator.getUserMedia = 
-	navigator.getUserMedia || navigator.webkitGetUserMedia || 
-	navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    navigator.getUserMedia || navigator.webkitGetUserMedia || 
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
 navigator.getUserMedia({audio : true}, onSuccess, onError);
 
 function onSuccess(stream){
-	console.log("onSuccess");
+    console.log("onSuccess");
 
-	document.querySelector("audio").src = URL.createObjectURL(stream);
-	var audioContext = new AudioContext();
-	var analyser     = audioContext.createAnalyser();
-	var timeDomain   = new Float32Array(analyser.frequencyBinCount);
-	var frequency    = new Uint8Array(analyser.frequencyBinCount);
-	audioContext.createMediaStreamSource(stream).connect(analyser);
+    //document.querySelector("audio").src = URL.createObjectURL(stream);
+    // ↑old. ↓new
+    document.querySelector("audio").src =
+	window.URL.createObjectURL(new Blob([stream], {type: "application/zip"}))
 
-	//==========
-	// 拍手を検出するクラス
-	// 第一引数:次の拍手までの待機時間(ミリ秒)
-	// 第二引数:反応する拍手音量の閾値
-	var cManager = new CrapManager(1000, 100);
+    
+    var audioContext = new AudioContext();
+    var analyser     = audioContext.createAnalyser();
+    var timeDomain   = new Float32Array(analyser.frequencyBinCount);
+    var frequency    = new Uint8Array(analyser.frequencyBinCount);
+    audioContext.createMediaStreamSource(stream).connect(analyser);
 
-	loop();
-	function loop(){
-		// Analiser
-		analyser.getFloatTimeDomainData(timeDomain);
-		analyser.getByteFrequencyData(frequency);
+    //==========
+    // 拍手を検出するクラス
+    // 第一引数:次の拍手までの待機時間(ミリ秒)
+    // 第二引数:反応する拍手音量の閾値
+    var cManager = new CrapManager(1000, 100);
 
-		// Stats
-		stats.update();
+    loop();
+    function loop(){
+	// Analiser
+	analyser.getFloatTimeDomainData(timeDomain);
+	analyser.getByteFrequencyData(frequency);
+	
+	// Stats
+	stats.update();
 
-		// 拍手の判定
-		if(cManager.trigger(timeDomain, frequency) == true){
-			// Cubeを追加する
-			addCube();
-		}
-
-		// Render
-		renderer.render(scene, camera);
-		// Request
-		window.requestAnimationFrame(loop);
+	// 拍手の判定
+	if(cManager.trigger(timeDomain, frequency) == true){
+	    // Cubeを追加する
+	    addCube();
 	}
+	
+	// Render
+	renderer.render(scene, camera);
+	// Request
+	window.requestAnimationFrame(loop);
+    }
 }
 
 function onError(e){
-	console.log("onError:" + e);
+    console.log("onError:" + e);
 }
 
 //==========
 // Cubeをランダムで配置する関数
 function addCube(){
-	var x = getRandom(-80, 80);
-	var y = getRandom(0, 80);
-	var z = getRandom(-80, 80);
-	var cube = createCube(5, 0xffffff, x, y, z);
-	scene.add(cube);
+    var x = getRandom(-80, 80);
+    var y = getRandom(0, 80);
+    var z = getRandom(-80, 80);
+    var cube = createCube(5, 0xffffff, x, y, z);
+    scene.add(cube);
 }
 
 //==========
 // Cubeを指定のサイズ、色、座標で作り出す関数
 function createCube(size, color, x, y, z){
-	var geometry = new THREE.BoxBufferGeometry(size, size, size);
-	var material = new THREE.MeshBasicMaterial({color: 0xccffcc, wireframe: true});
-	var mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x, y, z);
-	return mesh;
+    var geometry = new THREE.BoxBufferGeometry(size, size, size);
+    var material = new THREE.MeshBasicMaterial({color: 0xccffcc, wireframe: true});
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    return mesh;
 }
 
 function getRandom(min, max){
-	return Math.floor(Math.random()*(max-min+1))+min;
+    return Math.floor(Math.random()*(max-min+1))+min;
 }
 
 //==========
 // CrapManager
 class CrapManager{
-	constructor(interTime, interThreshold){
-		this.interTime      = interTime;
-		this.interThreshold = interThreshold;
-		this.interFlg       = false;
+    constructor(interTime, interThreshold){
+	this.interTime      = interTime;
+	this.interThreshold = interThreshold;
+	this.interFlg       = false;
+    }
+    trigger(timeDomain, frequency){
+	if(this.interFlg == true) return false;
+	var offset = 30;
+	var total  = Math.floor(frequency.length / offset);
+	var volume = 0;
+	for(var i=0; i<total; i+=offset){
+	    volume += frequency[i * offset];
 	}
-	trigger(timeDomain, frequency){
-		if(this.interFlg == true) return false;
-		var offset = 30;
-		var total  = Math.floor(frequency.length / offset);
-		var volume = 0;
-		for(var i=0; i<total; i+=offset){
-			volume += frequency[i * offset];
-		}
-		if(volume < this.interThreshold) return false;
-		this.interFlg = true;
-		setInterval(()=>{this.interFlg = false;}, this.interTime);
-		return true;
-	}
+	if(volume < this.interThreshold) return false;
+	this.interFlg = true;
+	setInterval(()=>{this.interFlg = false;}, this.interTime);
+	return true;
+    }
 }
